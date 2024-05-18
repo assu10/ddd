0714de6f00e05669fc4aab17006e104709324330
assu10 <juhyun1010@naver.com>
2024-05-18 18:47:11 +0900

    no message

--- src/main/java/com/assu/study/order/domain/OrderState.java
+++ src/main/java/com/assu/study/order/domain/OrderState.java
@@ -2,10 +2,10 @@ package com.assu.study.order.domain;
 
 // 주문 상태 표현
 public enum OrderState {
-    PAYMENT_WAITING,
-    PREPARING,
-    SHIPPED,
-    DELIVERING,
-    DELIVERY_COMPLETED,
-    CANCELED;
+    PAYMENT_WAITING,    // 결제 대기중
+    PREPARING,  // 상품 준비중
+    SHIPPED,    // 출고 완료
+    DELIVERING, // 배송 중
+    DELIVERY_COMPLETED, // 배송 완료
+    CANCELED;   // 주문 취소
 }
