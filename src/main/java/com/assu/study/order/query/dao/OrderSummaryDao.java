a69b5742105f55e31e6184b359521e483d58d8f6
assu10 <juhyun1010@naver.com>
2024-06-17 14:50:20 +0900

    5. 동적 인스턴스 생성

--- src/main/java/com/assu/study/order/query/dao/OrderSummaryDao.java
+++ src/main/java/com/assu/study/order/query/dao/OrderSummaryDao.java
@@ -1,8 +1,10 @@
 package com.assu.study.order.query.dao;
 
 import com.assu.study.order.query.dto.OrderSummary;
+import com.assu.study.order.query.dto.OrderView;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
@@ -19,4 +21,16 @@ public interface OrderSummaryDao extends Repository<OrderSummary, String> {
     List<OrderSummary> findAll(Specification<OrderSummary> spec);
 
     List<OrderSummary> findAll(Specification<OrderSummary> spec, Sort sort);
+
+    @Query("""
+            select new com.assu.study.order.query.dto.OrderView(
+            o.number, o.state, m.name, m.id, p.name
+            )
+            from Order o join o.orderLines ol, Member m, Product  p
+            where o.orderer.memberId.id = :ordererId 
+            and index(ol) = 0 
+            and ol.productId.id = p.id 
+            order by o.number.number desc
+            """)
+    List<OrderView> findOrderView(String ordererId);
 }
---
+++ src/main/java/com/assu/study/order/query/dto/OrderView.java
@@ -0,0 +1,23 @@
+package com.assu.study.order.query.dto;
+
+import com.assu.study.member.command.domain.MemberId;
+import com.assu.study.order.command.domain.OrderNo;
+import com.assu.study.order.command.domain.OrderState;
+import lombok.Getter;
+
+@Getter
+public class OrderView {
+    private final String number;
+    private final OrderState state;
+    private final String memberName;
+    private final String memberId;
+    private final String productName;
+
+    public OrderView(OrderNo number, OrderState state, String memberName, MemberId memberId, String productName) {
+        this.number = number.getNumber();
+        this.state = state;
+        this.memberName = memberName;
+        this.memberId = memberId.getId();
+        this.productName = productName;
+    }
+}
