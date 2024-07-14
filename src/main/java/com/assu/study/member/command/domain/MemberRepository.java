d4d76682b3ee9ba49664d0070d50fd208892d9d6
assu10 <juhyun1010@naver.com>
2024-07-14 10:54:19 +0900

    2. 선점 잠금 (비관적 잠금, Pessimistic Lock): `LockModeType.PESSIMISTIC_WRITE`, `@Lock`

--- src/main/java/com/assu/study/member/command/domain/MemberRepository.java
+++ src/main/java/com/assu/study/member/command/domain/MemberRepository.java
@@ -1,9 +1,24 @@
 package com.assu.study.member.command.domain;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends Repository<Member, MemberId> {
     Optional<Member> findById(MemberId memberId);
+
+    // 선점 잠금 방식 이용
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    // 잠금을 구하는 대기 시간을 2초로 지정
+    @QueryHints({
+            @QueryHint(name = "javax.persistenct.lock.timeout", value = "2000")
+    })
+    @Query("select m from Member m where m.id = :id")
+    Optional<Member> findByIdForUpdate(@Param("id") MemberId memberId);
 }
