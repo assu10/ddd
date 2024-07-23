60e72e63b2ae8b02ea07e4923a6ef15819d4bc16
assu10 <juhyun1010@naver.com>
2024-07-23 18:47:47 +0900

    3. 이벤트 적용 시 추가 고려 사항

--- src/main/java/com/assu/study/order/infra/OrderCanceledEventHandler.java
+++ src/main/java/com/assu/study/order/infra/OrderCanceledEventHandler.java
@@ -3,9 +3,10 @@ package com.assu.study.order.infra;
 import com.assu.study.order.command.application.RefundService;
 import com.assu.study.order.command.domain.OrderCanceledEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 // OrderCanceledEvent 처리하는 핸들러
 @RequiredArgsConstructor
@@ -14,7 +15,11 @@ public class OrderCanceledEventHandler {
     private final RefundService refundService;
 
     @Async
-    @EventListener(OrderCanceledEvent.class)
+    //@EventListener(OrderCanceledEvent.class)
+    @TransactionalEventListener(
+            classes = OrderCanceledEvent.class,
+            phase = TransactionPhase.AFTER_COMMIT   // 트랜잭션 커밋이 성공한 뒤 핸들러 메서드 실행
+    )
     public void handle(OrderCanceledEvent event) {
         refundService.refund(event.getOrderNumber());
     }
